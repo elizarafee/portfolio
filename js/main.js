ec584ee90effc96cb5919a7e9fda14cc6d5502f9
Eliza Ahmed <elizarafee@gmail.com>
2020-04-21 23:48:34 +0600

    Fixed the Navbar

--- js/main.js
+++ js/main.js
@@ -43,4 +43,21 @@ $(document).ready(function(){
         }
     })
 
+    // Steady navbar
+    let nav_offset_top = $('.header-area').height()+50;
+    function navbarFixed(){
+        if($('.header-area').length){
+            $(window).scroll(function(){
+                let scroll = $(window).scrollTop();
+                if(scroll>=nav_offset_top){
+                    $('.header-area .main-menu').addClass('navbar_fixed');
+                }
+                else{
+                    $('.header-area .main-menu').removeClass('navbar_fixed');
+                }
+            })
+        }
+    }
+
+    navbarFixed();
 });
